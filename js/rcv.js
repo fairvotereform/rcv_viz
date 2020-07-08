f9ccaf97c62844a7323f67b2e0005685943d598d
cz <czawora@fairvote.org>
2020-07-08 13:35:09 -0400

    add moe string

--- js/rcv.js
+++ js/rcv.js
@@ -233,7 +233,7 @@ window.rcvInitialize = function(config) {
     var moeDiv = interactiveDiv.append("div")
       .attr("id", "rcv-moe")
       .attr("style", rcvConfig["moeString"] ? "visibility: visible;" : "visibility: hidden;")
-      .html(rcvConfig["moeString"] ? rcvConfig["moeString"] : "");
+      .html("moeString" in rcvConfig ? rcvConfig["moeString"] : "");
 
     var creditsDiv = interactiveDiv.append("div")
       .attr("id", "rcv-credits")
